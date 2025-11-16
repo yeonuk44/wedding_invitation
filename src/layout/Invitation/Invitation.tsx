b3f6359e7465b173dcd903db6e3f2b9efd31c827
yeonuk44 <yeonuk44@gmail.com>
2025-11-17 06:57:23 +0900

    Delete: Image Caption

--- src/layout/Invitation/Invitation.tsx
+++ src/layout/Invitation/Invitation.tsx
@@ -10,7 +10,7 @@ const Invitation = () => {
     <InvitationWrapper>
       <Paragraph>{greeting.message}</Paragraph>
       <Host />
-      <Caption textAlign={'center'}>{greeting.eventDetail}</Caption>
+      {/* <Caption textAlign={'center'}>{greeting.eventDetail}</Caption> */}
       {/* TODO: 구글캘린더 추가하기 기능을 넣는다면 링크 수정 */}
       {/* <RoundButton
         target="_blank"
