14df6aacc23ac4e984bfdc9e2d5fa95c4afd3f18
yeonuk44 <yeonuk44@gmail.com>
2025-11-17 06:56:13 +0900

    Delete: Google Calendar function

--- src/layout/Invitation/Invitation.tsx
+++ src/layout/Invitation/Invitation.tsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 import data from 'data.json';
 import Host from '../Contact/Host.tsx';
-import RoundButton from '@/components/RoundButton.tsx';
+// import RoundButton from '@/components/RoundButton.tsx';
 import { Caption, Paragraph } from '@/components/Text.tsx';
 
 const Invitation = () => {
@@ -12,12 +12,12 @@ const Invitation = () => {
       <Host />
       <Caption textAlign={'center'}>{greeting.eventDetail}</Caption>
       {/* TODO: 구글캘린더 추가하기 기능을 넣는다면 링크 수정 */}
-      <RoundButton
+      {/* <RoundButton
         target="_blank"
         href=""
         rel="noreferrer">
         구글 캘린더 추가하기
-      </RoundButton>
+      </RoundButton> */}
     </InvitationWrapper>
   );
 };
